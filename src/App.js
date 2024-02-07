f6ca6fbb860ef47ee6d6934939c57b432b0cd3a7
wacoo <wabaham9@gmail.com>
2024-02-07 11:39:11 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+ import { Box, Button, Container, Paper, Typography } from '@mui/material';
+const serviceList = ["Service 1", "Service 2", "Service 3"];
+ function App() {
+  //  sx={{background: 'tomato', height: '100vh' }}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container sx={{my: 4, textAlign: 'center', color: "primary.main" }}>
+      <Typography variant='h1'>
+        Services
+      </Typography>
+      <Typography variant='h2'>Overview</Typography>
+      <Box sx={{ pt: 4, display: 'flex', flexDirection: {xs: 'column', md: 'row'}, gap: 4, justifyContent: 'space-between'}}>
+      {
+        serviceList.map((service) => (
+          <Paper elevation={3} sx={{height: 'auto', my: 3, p: 2, textAlign: 'left'}}>
+            <Typography variant='h3' sx={{mb: 2}}>
+              {service}
+            </Typography>
+            <Typography variant='paragraph'>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </Typography>
+            <div>
+              <Button variant='contained' sx={{mt: 2}}>LEARN MORE...</Button>
+            </div>
+          </Paper>
+        ))
+      }
+      </Box>
+    </Container>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import { ThemeProvider } from '@emotion/react';
+import { createTheme } from '@mui/material';
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#4286F5',
+    },
+    secondary: {
+      main: '#ff00ff',
+    },
+  },
+  typography: {
+    h1: {
+      fontSize: '3rem',
+      fontWeight: 600,
+    },
+    h2: {
+      fontSize: '1.75rem',
+      fontWeight: 600,
+    },
+    h3: {
+      fontSize: '1.5rem',
+      fontWeight: '600'
+    },
+  },
+})
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+     <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
